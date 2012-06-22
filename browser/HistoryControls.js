3e957dd9e1b68889531e8a58056b99e61fcb1f85
Leigh Pauls <leigh.pauls@gmail.com>
2012-06-21 23:37:46 -0400

    Intgrated simple forward/back control form the browser into the monkey browser, long jumps will still mess up

--- browser/HistoryControls.js
+++ browser/HistoryControls.js
@@ -3,6 +3,7 @@ function HistoryControls(tabManager, socket) {
 	this.tabManager = tabManager;
 	this.socket = socket;
 
+	// handle the on-page history controls
 	$('.back-button')
 		.on('click', function() {
 			me.moveHistory(-1);
@@ -12,6 +13,40 @@ function HistoryControls(tabManager, socket) {
 		.on('click', function() {
 			me.moveHistory(1);
 		});
+
+	// handle the plain browser history controls
+	this.onHistoryPop = function(e) {
+		// figure out how far I moved
+		var dist = e.state.historyPoint - me.historyPoint;
+		me.moveHistory(dist);
+	};
+	this.onResetBackup = function(e) {
+		me.historyPoint = e.state.historyPoint;
+		window.onpopstate = me.onHistoryPop;
+	};
+
+	this.historyPoint = 0;
+	this.resetHistoryState();
+}
+
+
+
+/*
+* safely configures the history state to capture forward and back actions from the browser
+*/
+HistoryControls.prototype.resetHistoryState = function() {
+	// disable the event handler
+	window.onpopstate = this.onResetBackup;
+
+	window.history.replaceState({ historyPoint: this.historyPoint -1 },
+								"", "#" + (this.historyPoint -1));
+	window.history.pushState({ historyPoint: this.historyPoint },
+							 "", "#" + this.historyPoint);
+	window.history.pushState({ historyPoint: this.historyPoint + 1},
+							 "", "#" + (this.historyPoint + 1));
+
+	// back up to the right spot
+	window.history.back();
 }
 
 HistoryControls.prototype.moveHistory = function(dist) {
@@ -19,5 +54,7 @@ HistoryControls.prototype.moveHistory = function(dist) {
 		tabId: this.tabManager.getCurrentTabId(),
 		dist: dist
 	});
-		
+	
+	this.historyPoint += dist;
+	this.resetHistoryState();
 };
